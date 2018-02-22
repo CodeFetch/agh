5b2c2775bc3f97e2383cccb28cb0c54004e817a7
Enrico Mioso <mrkiko.rs@gmail.com>
2018-02-22 09:23:59 +0100

    These source files where, after all, meant to contain XMPP-related code.

--- xmpp.c
+++ xmpp.c
@@ -16,10 +16,24 @@ void xmpp_thread_init(gpointer data) {
 
 	ct->handlers = handlers_setup();
 
-	handler_register(ct->handlers, &xmpp_test_handler);
+	//handler_register(ct->handlers, &xmpp_test_handler);
 
 	aghservices_messaging_setup(ct);
 	handlers_init(ct->handlers, ct);
+
+	g_print("XMPP library init\n");
+	xmpp_initialize();
+
+	/* Create XMPP library context */
+	xstate->xmpp_log = xmpp_get_default_logger(XMPP_LEVEL_INFO);
+
+	/* First parameter is NULL since we don't provide our own memory allocator. */
+	xstate->xmpp_ctx = xmpp_ctx_new(NULL, xstate->xmpp_log);
+
+	xstate->xmpp_conn = xmpp_conn_new(xstate->xmpp_ctx);
+
+	xmpp_conn_set_jid(xstate->xmpp_conn, "mrkiko@jabber.linux.it");
+	xmpp_conn_set_pass(xstate->xmpp_conn, "dviselect_123_456");
 
 	return;
 }
@@ -28,10 +42,10 @@ gpointer xmpp_thread_start(gpointer data) {
 	struct agh_thread *ct = data;
 	struct xmpp_state *xstate = ct->thread_data;
 
-	g_print("XMPP started.\n");
-	g_usleep(8*G_USEC_PER_SEC);
-	g_print("%s: entering main loop\n",ct->thread_name);
-	g_main_loop_run(ct->evl);
+	xmpp_connect_client(xstate->xmpp_conn, NULL, 0, xmpp_connection_handler, xstate);
+
+	xmpp_run(xstate->xmpp_ctx);
+
 	return data;
 }
 
@@ -41,9 +55,113 @@ void xmpp_thread_deinit(gpointer data) {
 
 	g_print("XMPP deinit.\n");
 
+	xmpp_conn_release(xstate->xmpp_conn);
+	xmpp_ctx_free(xstate->xmpp_ctx);
+	xmpp_shutdown();
 	handlers_finalize(ct->handlers, ct);
 	handlers_teardown(ct->handlers);
 	g_free(ct->thread_data);
 	ct->handlers = NULL;
 	return;
+}
+
+void xmpp_connection_handler(xmpp_conn_t * const conn, const xmpp_conn_event_t status, const int error, xmpp_stream_error_t * const stream_error, void * const userdata) {
+	struct xmpp_state *xstate;
+	xmpp_stanza_t *pres;
+	xmpp_ctx_t *ctx;
+
+	xstate = userdata;
+
+	ctx = xstate->xmpp_ctx;
+	pres = NULL;
+
+	if (status == XMPP_CONN_CONNECT) {
+		xmpp_handler_add(conn, version_handler, "jabber:iq:version", "iq", NULL, xstate);
+		xmpp_handler_add(conn, message_handler, NULL, "message", NULL, xstate);
+		pres = xmpp_presence_new(ctx);
+		xmpp_send(xstate->xmpp_conn, pres);
+		xmpp_stanza_release(pres);
+		pres = NULL;
+	} else {
+		g_print("We are disconnected.\n");
+		xmpp_stop(ctx);
+	}
+	return;
+}
+
+int version_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata) {
+	xmpp_stanza_t *reply, *query, *name, *version, *text;
+	const char *ns;
+
+	struct xmpp_state *xstate;
+	xmpp_ctx_t *ctx;
+
+	xstate = userdata;
+	ctx = xstate->xmpp_ctx;
+
+	g_print("Received version request from %s\n", xmpp_stanza_get_from(stanza));
+	reply = xmpp_stanza_reply(stanza);
+	xmpp_stanza_set_type(reply, "result");
+
+	query = xmpp_stanza_new(ctx);
+	xmpp_stanza_set_name(query, "query");
+	ns = xmpp_stanza_get_ns(xmpp_stanza_get_children(stanza));
+
+	if (ns) {
+		xmpp_stanza_set_ns(query, ns);
+	}
+
+	name = xmpp_stanza_new(ctx);
+	xmpp_stanza_set_name(name, "name");
+	xmpp_stanza_add_child(query, name);
+	xmpp_stanza_release(name);
+
+	text = xmpp_stanza_new(ctx);
+	xmpp_stanza_set_text(text, "AGH");
+	xmpp_stanza_add_child(name, text);
+	xmpp_stanza_release(text);
+
+	version = xmpp_stanza_new(ctx);
+	xmpp_stanza_set_name(version, "version");
+	xmpp_stanza_add_child(query, version);
+	xmpp_stanza_release(version);
+
+	text = xmpp_stanza_new(ctx);
+	xmpp_stanza_set_text(text, "0.01");
+	xmpp_stanza_add_child(version, text);
+	xmpp_stanza_release(text);
+
+	xmpp_stanza_add_child(reply, query);
+	xmpp_stanza_release(query);
+	xmpp_send(conn, reply);
+	xmpp_stanza_release(reply);
+
+	return 1;
+}
+
+int message_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata) {
+	struct xmpp_state *xstate;
+	xmpp_ctx_t *ctx;
+	xmpp_stanza_t *body;
+	const char *type;
+	char *intext;
+
+	xstate = userdata;
+	ctx = xstate->xmpp_ctx;
+
+	body = xmpp_stanza_get_child_by_name(stanza, "body");
+	if (!body)
+		return 1;
+
+	type = xmpp_stanza_get_type(stanza);
+	if (type != NULL && g_strcmp0(type, "error") == 0)
+		return 1;
+
+	intext = xmpp_stanza_get_text(body);
+
+	g_print("Incoming message from %s: %s\n", xmpp_stanza_get_from(stanza), intext);
+
+	xmpp_free(ctx, intext);
+	
+	return 1;
 }
--- xmpp.h
+++ xmpp.h
@@ -1,15 +1,24 @@
 #ifndef __xmpp_h__
 #define __xmpp_h__
+#include <strophe.h>
 #include "agh.h"
 
 struct xmpp_state {
-	gint testval;
+	xmpp_ctx_t *xmpp_ctx;
+	xmpp_conn_t *xmpp_conn;
+	xmpp_log_t *xmpp_log;
+	char *jid;
+	char *pass;
 };
 
 void xmpp_thread_init(gpointer data);
 gpointer xmpp_thread_start(gpointer data);
 void xmpp_thread_deinit(gpointer data);
-gboolean xmpp_pass_message(gpointer data);
+
+/* libstrophe XMPP handlers */
+int version_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata);
+int message_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata);
+void xmpp_connection_handler(xmpp_conn_t * const conn, const xmpp_conn_event_t status, const int error, xmpp_stream_error_t * const stream_error, void * const userdata);
 
 static struct agh_thread xmpp_thread_ops = {
 	.thread_name = "XMPP",
