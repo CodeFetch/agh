8c29dbeb9960b44ec3bc39dad5be51824ce75bbe
Enrico Mioso <mrkiko.rs@gmail.com>
2019-01-23 14:44:42 +0100

    agh_logging.h: learned about __VA_ARGS__ existence

--- agh_logging.h
+++ agh_logging.h
@@ -1,11 +1,11 @@
 #ifndef __agh_logging_h__
 #define __agh_logging_h__
 
-#define agh_log(log_domain, log_level, message) \
-	g_log_structured(log_domain, log_level, "CODE_FILE", __FILE__, "CODE_LINE", __LINE__, "MESSAGE", message);
+#define agh_log(log_domain, log_level, message, ...) \
+	g_log_structured(log_domain, log_level, "CODE_FILE", __FILE__, "CODE_LINE", __LINE__, "MESSAGE", message, __VA_ARGS__);
 
-#define agh_log_dbg(log_domain, message) agh_log(log_domain, G_LOG_LEVEL_DEBUG, message)
-#define agh_log_info(log_domain, message) agh_log(log_domain, G_LOG_LEVEL_INFO, message)
+#define agh_log_dbg(log_domain, message, ...) agh_log(log_domain, G_LOG_LEVEL_DEBUG, message, __VA_ARGS__)
+#define agh_log_info(log_domain, message, ...) agh_log(log_domain, G_LOG_LEVEL_INFO, message, __VA_ARGS__)
 
 GLogWriterOutput
 g_agh_log_writer (GLogLevelFlags   log_level,
