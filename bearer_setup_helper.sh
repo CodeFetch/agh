82a3a863d3011768d3c72ec80b2129c8a381a506
Enrico Mioso <mrkiko.rs@gmail.com>
2019-04-12 18:16:52 +0200

    bearer_setup_helper.sh: rewrite using protocol handler
    agh.proto: a rudimentary protocol handler
    
    This is a best effort implementation of a protocol handler. I am not aware of bad consequences when running this code, but that's all I can say. :)
    I noticed failures like:
    Fri Apr 12 16:04:58 2019 daemon.notice netifd: agh_mm (20477): Command failed: Permission denied
    during teardown.
    
    How to use: place in your OpenWrt system's /etc/config/network something like:
    
    config interface agh_mm
    	option proto 'agh'
    	option auto     0
    	option environment_fragments    "/tmp/bearers_descriptions"
    
    If you would like to replace the path where bearers descriptions are stored, be sure to update also bearer_setup_helper.sh accordingly.
    Note that this location will be subject to possibly frequent writes. Storing these files in flash memory is not recommended, and maybe not so useful... :)
    Much of this code comes from the Aleksander Morgado's ModemManager OpenWrt integration work, available at:
    https://gitlab.freedesktop.org/mobile-broadband/mobile-broadband-openwrt
    
    all the bugs and changes are my responsibility. We are very thankful to him.

--- bearer_setup_helper.sh
+++ bearer_setup_helper.sh
@@ -1,202 +1,51 @@
 #!/bin/sh
 
-# This file is under GPL V2 or later, at your option.
+BEARERS_DESCRIPTIONS_BASE_DIR="/tmp/"
+BEARERS_DESCRIPTIONS_PATH="${BEARERS_DESCRIPTIONS_BASE_DIR}/bearers_descriptions"
+CURRENT_BFILE=""
 
-# Why not a protocol handler? And what about using "fw3 -q reload" ?
+# . /tmp/data_model
 
-. /lib/functions.sh
-. /usr/share/libubox/jshn.sh
-
-[ -x /usr/bin/readlink ] || exit 0
-
-PATH="/usr/sbin:/usr/bin:/sbin:/bin"
-
-# test
-# . ./mydata_down
-
-# Copied directly from ModemManager protocol handler by Aleksander Morgado
-cdr2mask ()
-{
-	# Number of args to shift, 255..255, first non-255 byte, zeroes
-	set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
-	[ $1 -gt 1 ] && shift $1 || shift
-	echo ${1-0}.${2-0}.${3-0}.${4-0}
-}
-
-get_intfname() {
-	(
-	echo "mm_$(readlink /proc/self)"
-	)
-}
-
-process_bearer() {
-	if [ "${BEARER_INTERFACE}" != "unknown" ] ; then
-		echo "Bearer is coming up"
-		add_interface
+bearer_descriptions_dir_init() {
+	if [ ! -d "${BEARERS_DESCRIPTIONS_PATH}" ]; then
+		echo "Creating beaarer descriptions directory at ${BEARERS_DESCRIPTIONS_PATH}"
+		mkdir "${BEARERS_DESCRIPTIONS_PATH}"
 	else
-		echo "Bearer was going down."
-		search_and_remove_interface teardown_interface
+		echo "Base directory already exists."
 	fi
 }
 
-add_interface() {
-	local exit_status=0
-	local uci_network_intf=""
-	local logical_intf
+process_bearer_data() {
+	BFILE="${BEARERS_DESCRIPTIONS_PATH}/bb$(basename ${BEARER_PATH})"
+	CURRENT_BFILE="${BFILE}"
 
-	# Does interface exist?
-	while [ "$exit_status" -eq 0 ]; do
-		logical_intf=$(get_intfname)
-		# logical_intf="mmtest"
-		if [ "$(uci_get network $logical_intf)" = "interface" ]; then
-			echo "It does, and is an interface"
-			check_and_maybe_destroy_intf "$logical_intf"
-			exit_status=$?
-		else
-			exit_status=1
+	echo "Processing bearer data for ${BEARER_PATH}"
+
+	if [ "${BEARER_INTERFACE}" == "unknown" ]; then
+		rm "${BFILE}"
+	else
+		set | grep 'BEARER_' >"${BFILE}"
+	fi
+}
+
+update_bearers_descriptions() {
+	ls "${BEARERS_DESCRIPTIONS_PATH}" | while read a; do
+		if [ "${BEARERS_DESCRIPTIONS_PATH}"/"${a}" == "${CURRENT_BFILE}" ]; then
+			echo "skipping ${a}"
+			continue
+		fi
+		. "${BEARERS_DESCRIPTIONS_PATH}"/"${a}"
+		BCONNECTED=$(mmcli -b "${BEARER_PATH}" -K | tr -d ' ' | grep 'bearer.status.connected:' | cut -d ':' -f 2)
+		if [ "${BCONNECTED}" != "yes" ]; then
+			echo "Deleting $a obsolescent bearer description."
+			rm "${BEARERS_DESCRIPTIONS_PATH}"/"${a}"
 		fi
 	done
-
-	echo "New logical interface: $logical_intf"
-
-	case "$BEARER_IP_METHOD" in
-		ppp) echo "ppp connection method support to be implemented";;
-		static) add_interface_static "$logical_intf" ;;
-		DHCP) add_interface_dhcp "$logical_intf" ;;
-		*) echo "Unknown connection method $BEARER_IP_METHOD";;
-	esac
 }
 
-add_interface_static() {
-	local logical_intf_name="$1"
-
-	add_interface_common "$logical_intf_name"
-
-	uci_set network "$logical_intf_name" proto static
-	uci_set network "$logical_intf_name" ipaddr "${BEARER_IP_ADDRESS}"
-	uci_set network "$logical_intf_name" netmask $(cdr2mask "${BEARER_IP_PREFIX}")
-	uci_set network "$logical_intf_name" gateway "${BEARER_IP_GATEWAY}"
-
-	# we may be interested in ip6addr, ip6ifaceid, ip6gw, ip6assign, ip6hint, ip6prefix and ip6class
-
-	# Sorry, haven't found a way to do this via uci shell API
-	uci add_list network."$logical_intf_name".dns="${BEARER_IP_DNS_1}"
-	uci add_list network."$logical_intf_name".dns="${BEARER_IP_DNS_2}"
-
-	# We may be interested in modifying the metric
-
-	uci_set network "$logical_intf_name" metric 100
-
-	interface_complete_common "$logical_intf_name"
-
-}
-
-add_interface_dhcp() {
-	local logical_intf_name="$1"
-
-	add_interface_common "$logical_intf_name"
-
-	uci_set network "$logical_intf_name" proto dhcp
-
-	uci_set network "$logical_intf_name" metric 100
-
-	interface_complete_common "$logical_intf_name"
-
-}
-
-add_interface_common() {
-	echo "Welcome, add_interface_common"
-
-	uci_add network interface "$logical_intf_name"
-
-	uci_set network "$logical_intf_name" bearer_path "${BEARER_PATH}"
-	uci_set network "$logical_intf_name" ifname "${BEARER_INTERFACE}"
-	uci_set network "$logical_intf_name" mtu "${BEARER_IP_MTU}"
-	uci_set network "$logical_intf_name" auto 0
-
-	# we may be interested in setting macaddr, ipv6 and ip4table + ip6table for specific routes IPV4 / IPV6 tables ??
-}
-
-interface_complete_common() {
-	local logical_intf_name="$1"
-
-	ubus call network reload
-
-	echo "ifup $logical_intf_name"
-	ifup "$logical_intf_name"
-	uci add_list "firewall.@zone[1].network=$logical_intf_name"
-	/etc/init.d/firewall restart >/dev/null 2>&1
-}
-
-# exit values: 1=success, 0=failure
-check_and_maybe_destroy_intf() {
-	local logical_intfname="$1"
-	local bpath
-	local exit_status=0
-	local bcheck_exitstatus=0
-
-	bpath="$(uci_get network $logical_intfname bearer_path not_present)"
-
-	if [ "$bpath" = "not_present" ]; then
-		echo "$1 does not seem to be one of our interfaces"
-		return $exit_status
-	fi
-
-	json_init
-
-	json_load "$(ifstatus $logical_intfname)"
-
-	json_get_var is_up up
-
-	if [ $is_up -eq 1 ]; then
-		mmcli -b "$bpath" >/dev/null 2>&1
-		bcheck_exitstatus=$?
-		if [ $bcheck_exitstatus -eq 1 ]; then
-			echo "ifdown of $logical_intfname stale interface"
-			ifdown "$logical_intfname"
-			json_load "$(ifstatus $logical_intfname)"
-			json_get_var is_up up
-		fi
-	fi
-
-	if [ $is_up -eq 0 ]; then
-		echo "Cleaning up $logical_intfname stale interface"
-		uci_remove network "$logical_intfname"
-		uci del_list "firewall.@zone[1].network=$logical_intfname"
-		ubus call network reload
-		/etc/init.d/firewall restart >/dev/null 2>&1
-	else
-		exit_status=1
-	fi
-
-	json_cleanup
-
-	return $exit_status
-}
-
-search_and_remove_interface() {
-	config_load network
-	config_foreach "$1" interface
-}
-
-teardown_interface() {
-	local bpath
-	local current_interface="$1"
-
-	bpath="$(uci_get network $current_interface bearer_path)"
-	if [ ! -z "$bpath" ]; then
-		if [ "$BEARER_PATH" = "$bpath" ]; then
-			echo "ifdown $current_interface"
-			ifdown "$current_interface"
-			uci del_list "firewall.@zone[1].network=$current_interface"
-			uci_remove network "$current_interface"
-			ubus call network reload
-			/etc/init.d/firewall restart >/dev/null 2>&1
-		fi
-	fi
-}
-
-if [ ! -z ${BEARER_PATH} ]; then
-	search_and_remove_interface check_and_maybe_destroy_intf
-	process_bearer
+if [ ! -z "${BEARER_PATH}" ]; then
+	bearer_descriptions_dir_init
+	process_bearer_data
+	update_bearers_descriptions
+	ifup agh_mm
 fi
